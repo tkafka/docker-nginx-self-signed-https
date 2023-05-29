14f9e9db819962d6b80407c6c63c6fc82deca730
Tomáš Kafka <tk@tomaskafka.com>
2023-05-29 11:36:20 +0200

    Add support for TIMEOUTS_S, CLIENT_MAX_BODY_SIZE, CLIENT_BODY_BUFFER_SIZE

--- run.sh
+++ run.sh
@@ -3,5 +3,8 @@
 sed -i -e "s|XX_REMOTE_URL_XX|$REMOTE_URL|g" /etc/nginx/conf.d/default.conf
 sed -i -e "s|XX_HTTP_PORT_XX|$HTTP_PORT|g" /etc/nginx/conf.d/default.conf
 sed -i -e "s|XX_HTTPS_PORT_XX|$HTTPS_PORT|g" /etc/nginx/conf.d/default.conf
+sed -i -e "s|XX_TIMEOUTS_S_XX|$TIMEOUTS_S|g" /etc/nginx/conf.d/default.conf
+sed -i -e "s|XX_CLIENT_MAX_BODY_SIZE_XX|$CLIENT_MAX_BODY_SIZE|g" /etc/nginx/conf.d/default.conf
+sed -i -e "s|XX_CLIENT_BODY_BUFFER_SIZE_XX|$CLIENT_BODY_BUFFER_SIZE|g" /etc/nginx/conf.d/default.conf
 
 exec nginx -g "daemon off;"
